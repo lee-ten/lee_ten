cca7781b26ca8c313f260b3bb03350558b9baf83
li.jiuyi@yanwei365.com <li.jiuyi@yanwei365.com>
2019-02-28 14:03:46 +0800

    更新项目名称

---
+++ java/src/main/java/JavaOrderApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOrderApplication.class, args);
+	}
+
+}
